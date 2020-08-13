17d803231054e7ac98c9858f824f4affcde532c5
royFloresNyc <royfloresnyc@gmail.com>
2020-08-13 17:57:56 -0400

    Complete Lab

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.characters.map{|char| char.actor.full_name }
+    end 
   
 end
